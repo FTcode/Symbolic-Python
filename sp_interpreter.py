125f85c5a2057927c84a20ee863e61df02c4a0a4
FTcode <filip.trenkic@gmail.com>
2017-10-24 19:00:07 +0100

    Added interpreter from wiki

---
+++ sp_interpreter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2
+"""
+sypy.py: Example interpreter for Symbolic Python.
+Call with command line arguments: <script-file> [input]
+"""
+
+import sys
+import ast
+
+script = open(sys.argv[1]).read()
+
+if len(sys.argv) > 2:
+  _ = ast.literal_eval(sys.argv[2])
+else:
+  _ = None
+
+for char in script:
+  if char.isalnum():
+    print("Invalid script: the %r character is banned." % char)
+    quit()
+
+def __(cmd):
+  for i,j in globals().items(): locals()[i] = j
+  exec cmd
+  del cmd
+  for i,j in locals().items(): globals()[i] = j
+
+exec script
+
+print _
